e4b9fcdba4861613db000f7cdd9afff34f64faae
AlishaAroob <aroob.dev@gmail.com>
2024-04-30 21:10:14 +0500

    added favicon

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -3,24 +3,33 @@ import logo from "./Assets/logo1.png";
 export default function Navbar() {
   return (
     <div>
-      <nav>
+      <nav className="sticky">
         <div className=" h-13 w-100% flex p-3  bg-purple-800 mx-auto justify-evenly">
           <span className="flex justify-between space-x-2">
-            <img className="h-8 w-8 my-2" src={logo} alt="" />
+            <img className="h-9 w-9 my-1" src={logo} alt="" />
             <h1 className=" font-bold text-white my-2 ">Elite Emporium</h1>
           </span>
+          <i className="bx bx-menu my-2 font-bold text-white text-lg hidden"></i>
           <ul className="flex text-white my-2 font-bold space-x-5 font-sans px-5">
             <li>
-              <a href="/">Home</a>
+              <a href="/" className="hover:underline ">
+                Home
+              </a>
             </li>
             <li>
-              <a href="/">About</a>
+              <a href="/" className="hover:underline">
+                About
+              </a>
             </li>
             <li>
-              <a href="/">Shop</a>
+              <a href="/" className="hover:underline">
+                Shop
+              </a>
             </li>
             <li>
-              <a href="/">Contact Us</a>
+              <a href="/" className="hover:underline">
+                Contact Us
+              </a>
             </li>
           </ul>
           <ul className="space-x-6">
