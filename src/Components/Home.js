02e0609af3cc60f3c0b7087d092a0bd5df36c199
AlishaAroob <aroob.dev@gmail.com>
2024-05-06 22:41:12 +0500

    Styling Home Section

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,9 @@
 import React from "react";
-
+import Cover from "./Assets/Cover.jpg";
 export default function Home() {
   return (
-    <div className="Section h-96 bg-fuchsia-800">
+    <div className="Section h-96">
+      <img className="h-96 bg-opacity-50 w-screen bg" src={Cover} alt="" />
       <div className="grid grid-cols-2 ">
         <h1 className="font-bold text-white text-left text-xl  ">
           Welcome to Elite Emporium
