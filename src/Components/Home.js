c944366306440555f241e5149ba4e4450df9979f
AlishaAroob <aroob.dev@gmail.com>
2024-05-09 11:30:12 +0500

    Styling Home Section

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,8 +1,12 @@
 import React from "react";
 import Cover from "./Assets/Cover.jpg";
+
 export default function Home() {
   return (
-    <div className="Section h-96 bg-cover ">
+    <div
+      className="Section h-96 bg-cover"
+      style={{ backgroundImage: `url(${Cover})` }}
+    >
       <h1 className="text-black text-2xl font-bold">
         Welcome to Elite Emporium
       </h1>
