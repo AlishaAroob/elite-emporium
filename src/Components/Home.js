f9f41771d7bff16ab5ec51d286e87d410dd8bb8b
AlishaAroob <aroob.dev@gmail.com>
2024-05-11 19:30:48 +0500

    Styling Home Section

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,23 +4,23 @@ import Book from "./Assets/Book.png";
 export default function Home() {
   return (
     <div
-      className="Section h-96 max-h-full bg-cover flex "
+      className="Section h-96 bg-cover flex shadow-lg"
       style={{ backgroundImage: `url(${Cover})` }}
     >
-      <div className="p-20 text-left">
+      <div className="p-20 text-left align-middle">
         <h1 className="text-5xl font-bold  text-white">
           Welcome to Elite Emporium!
         </h1>
-        <p className="text-lg text-white my-2">
+        <p className="text-lg text-white my-2 text-center ">
           We're your go-to spot for books online.
         </p>
-        <p className="text-lg text-white ">
+        <p className="text-lg text-white text-center ">
           Find all kinds of great reads, from old favorites to new discoveries.
         </p>
-        <p className="text-lg text-white ">
+        <p className="text-lg text-white text-center ">
           Start your reading adventure with us now!
         </p>
-        <button className="text-white box-border text-lg border-spacing-1 bg-yellow-500 rounded-lg p-2 w-56 hover:bg-yellow-400 ">
+        <button className="text-white box-border text-lg border-spacing-1 bg-yellow-500 rounded-lg  mx-48 my-2 p-2 w-56 hover:bg-yellow-400 ">
           Order Now <i className="bx bx-cart text-white text-lg font-bold " />
         </button>
       </div>
