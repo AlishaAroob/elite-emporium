cb7fa8f2588ea2a8f1f1cbc608c04df5120903a6
AlishaAroob <aroob.dev@gmail.com>
2024-05-12 10:32:10 +0500

    Styling Best Selling

--- src/Components/BestSelling.js
+++ src/Components/BestSelling.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Book from "./Assets/Book.png";
 import Book1 from "./Assets/Book1.png";
+import Book2 from "./Assets/Book2.png";
+import Book3 from "./Assets/Book3.png";
+import Book4 from "./Assets/Book4.png";
+
 export default function BestSelling() {
   return (
     <div className="Section1 h-96 w-full font-bold">
@@ -10,6 +14,15 @@ export default function BestSelling() {
       </div>
       <div>
         <img className=" w-64 ml-8 p-10" src={Book1} alt="Book1" />
+      </div>
+      <div>
+        <img className=" w-64 ml-8 p-10" src={Book2} alt="Book2" />
+      </div>
+      <div>
+        <img className=" w-64 ml-8 p-10" src={Book3} alt="Book3" />
+      </div>
+      <div>
+        <img className=" w-64 ml-8 p-10" src={Book4} alt="Book4" />
       </div>
     </div>
   );
