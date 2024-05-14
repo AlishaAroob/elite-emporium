332b5b786befcca7d087d85b3f68def9f053724c
AlishaAroob <aroob.dev@gmail.com>
2024-05-14 12:50:52 +0500

    Created Pricing Section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import BestSelling from "./Components/BestSelling";
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
+import Pricing from "./Components/Pricing";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Home />
       <BestSelling />
+      <Pricing />
     </>
   );
 }
