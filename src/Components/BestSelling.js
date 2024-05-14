218df4b46c44d6394473022f7df2ed570c13d448
AlishaAroob <aroob.dev@gmail.com>
2024-05-14 12:12:07 +0500

    Styling Bestseller

--- src/Components/BestSelling.js
+++ src/Components/BestSelling.js
@@ -7,15 +7,18 @@ import Book4 from "./Assets/Book4.png";
 
 export default function BestSelling() {
   return (
-    <div className="Section1 max-h-max w-full font-bold  bg-teal-700 shadow-lg ">
-      <h1 className="text-4xl text-white text-center py-5 font">
+    <div className="Section1 max-h-max w-full font-bold  shadow-lg ">
+      <h1 className="text-4xl text-black text-center py-5 font">
         {" "}
         Best Sellers
       </h1>
-      <div className="Grid grid-rows-5 bg-black  justify-around">
+      <div className="Grid grid-rows-5  justify-between">
         <div className="flex  mt-5 justify-around ">
           <div>
             <img className=" w-64 px-5" src={Book} alt="Book" />
+            <h1 className="text-lg mx-4 "> Soul </h1>
+            <p className="text-sm mx-4">OLIVIA WILSON</p>
+            <p className="text-sm mx-4">$29</p>
             <button className="text-white mx-4 box-border  text-lg border-spacing-1 transition ease-in-out delay-150 bg-yellow-500 rounded-lg   my-3 p-2 w-56 hover:-translate-y-1 hover:scale-110 hover:bg-yellow-400 duration-300 ">
               Add to cart
               <i className=" mx-2 bx bx-cart text-white text-lg font-bold " />
@@ -23,6 +26,9 @@ export default function BestSelling() {
           </div>
           <div>
             <img className=" w-64  px-5" src={Book1} alt="Book1" />
+            <h1 className="text-lg mx-4 ">Thread </h1>
+            <p className="text-sm mx-4">OLIVIA WILSON</p>
+            <p className="text-sm mx-4">$35</p>
             <button className="text-white mx-4  box-border text-lg border-spacing-1 transition ease-in-out delay-150 bg-yellow-500 rounded-lg   my-3 p-2 w-56 hover:-translate-y-1 hover:scale-110 hover:bg-yellow-400 duration-300 ">
               Add to cart
               <i className=" mx-2 bx bx-cart text-white text-lg font-bold " />
@@ -30,6 +36,9 @@ export default function BestSelling() {
           </div>
           <div>
             <img className=" w-64 px-5" src={Book2} alt="Book2" />
+            <h1 className="text-lg mx-4 ">The Young Wizard</h1>
+            <p className="text-sm mx-4">AARON LOEB</p>
+            <p className="text-sm mx-4">$40</p>
             <button className="text-white mx-4 box-border text-lg border-spacing-1 transition ease-in-out delay-150 bg-yellow-500 rounded-lg   my-3 p-2 w-56 hover:-translate-y-1 hover:scale-110 hover:bg-yellow-400 duration-300 ">
               Add to cart
               <i className=" mx-2 bx bx-cart text-white text-lg font-bold " />
@@ -37,6 +46,9 @@ export default function BestSelling() {
           </div>
           <div>
             <img className=" w-64 px-5" src={Book3} alt="Book3" />
+            <h1 className="text-lg mx-4 ">Walk into the shadow</h1>
+            <p className="text-sm mx-4">ESTELLE DARCY</p>
+            <p className="text-sm mx-4">$45</p>
             <button className="text-white mx-4 box-border text-lg border-spacing-1 transition ease-in-out delay-150 bg-yellow-500 rounded-lg   my-3 p-2 w-56 hover:-translate-y-1 hover:scale-110 hover:bg-yellow-400 duration-300 ">
               Add to cart
               <i className=" mx-2 bx bx-cart text-white text-lg font-bold " />
@@ -44,6 +56,9 @@ export default function BestSelling() {
           </div>
           <div>
             <img className=" w-64 px-5" src={Book4} alt="Book4" />
+            <h1 className="text-lg mx-4 ">Spear</h1>
+            <p className="text-sm mx-4">NEIL TRAN</p>
+            <p className="text-sm mx-4">$25</p>
             <button className="text-white mx-4 box-border text-lg border-spacing-1 transition ease-in-out delay-150 bg-yellow-500 rounded-lg   my-3 p-2 w-56 hover:-translate-y-1 hover:scale-110 hover:bg-yellow-400 duration-300 ">
               Add to cart
               <i className=" mx-2 bx bx-cart text-white text-lg font-bold " />
