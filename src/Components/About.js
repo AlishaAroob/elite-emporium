8b08f8d86377dd9aabf27c80ad094af5b6d76937
AlishaAroob <aroob.dev@gmail.com>
2024-05-20 11:23:15 +0500

    styling About us section

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,11 +5,14 @@ export default function About() {
     <div className="Section2 min-h-[600px]">
       <h1 className="text-4xl font-bold pt-5 text-center"> About Us </h1>
       <div className=" container grid grid-cols-2 bg-black border-zinc-300 text-white">
-        <p>
-          Welcome to Elite Emporium, your premier destination for all things
-          literary! At Elite Emporium, we're more than just a bookstore we're a
-          community of book lovers passionate about spreading the joy of reading
-          far and wide.
+        <p className="text-justify">
+          Welcome to Elite Emporium, your premier online bookstore where
+          literature meets luxury. At Elite Emporium, we believe that books are
+          more than just pages and ink; they are gateways to adventure,
+          knowledge, and inspiration. Our mission is to curate an exceptional
+          collection of books that cater to diverse tastes and preferences,
+          ensuring that every reader finds their next favorite read.
+          <br></br>
         </p>
       </div>
     </div>
