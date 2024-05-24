fdfa8e79dcd4f02093befd46fffd1ed732fbae0a
AlishaAroob <aroob.dev@gmail.com>
2024-05-24 22:29:21 +0500

    Styling Contact Us

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,5 +1,13 @@
 import React from "react";
-
+import logo1 from "./Assets/logo1.png";
 export default function Contact() {
-  return <div></div>;
+  return (
+    <div className="Section3 min-h-[500px] bg-yellow-600">
+      <h1 className="font-bold text-4xl text-center pt-8"> Contact Us</h1>
+      <span className="flex  space-x-2">
+        <img className="h-9 w-9 my-1" src={logo1} alt="" />
+        <h1 className="font-bold text-white my-2">Elite Emporium</h1>
+      </span>
+    </div>
+  );
 }
