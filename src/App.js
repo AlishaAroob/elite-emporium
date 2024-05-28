b6d6671943d0a05002dd3d93233903f7b9c79945
AlishaAroob <aroob.dev@gmail.com>
2024-05-28 10:31:51 +0500

    Created and designing Contact box

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import BestSelling from "./Components/BestSelling";
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 import About from "./Components/About";
+import Contact from "./Components/Contact";
 function App() {
   return (
     <>
@@ -9,6 +10,7 @@ function App() {
       <Home />
       <BestSelling />
       <About />
+      <Contact />
     </>
   );
 }
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,13 +1,13 @@
-// import React from "react";
-// import logo1 from "./Assets/logo1.png";
-// export default function Contact() {
-//   return (
-//     <div className="Section3 min-h-[500px] bg-yellow-600">
-//       <h1 className="font-bold text-4xl text-center pt-8"> Contact Us</h1>
-//       <span className="flex  space-x-2">
-//         <img className="h-9 w-9 my-1" src={logo1} alt="" />
-//         <h1 className="font-bold text-white my-2">Elite Emporium</h1>
-//       </span>
-//     </div>
-//   );
-// }
+import React from "react";
+import logo1 from "./Assets/logo1.png";
+export default function Contact() {
+  return (
+    <div className="Section3 min-h-[450px]  bg-teal-700 ">
+      <h1 className="font-bold text-4xl text-center pt-8"> Contact Us</h1>
+      <span className="flex  space-x-2 ml-20 mt-14 p-10">
+        <img className=" h-28 w-40 my-1" src={logo1} alt="" />
+        <h1 className="font-bold text-white my-10 text-lg">Elite Emporium</h1>
+      </span>
+    </div>
+  );
+}
