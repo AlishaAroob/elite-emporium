e8c52fc3cff297d354484dac542aadbe5da1196c
AlishaAroob <aroob.dev@gmail.com>
2024-05-29 19:10:25 +0500

    styling contact us

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,8 +3,11 @@ import logo1 from "./Assets/logo1.png";
 export default function Contact() {
   return (
     <div className="Section3 min-h-[450px]  bg-teal-700 ">
-      <h1 className="font-bold text-4xl text-center pt-8"> Contact Us</h1>
-      <span className="flex  space-x-2 ml-20 mt-14 p-10">
+      <h1 className="font-bold text-4xl text-center pt-8 text-white ">
+        {" "}
+        Contact Us
+      </h1>
+      <span className="flex  space-x-2 ml-20 mt-10 p-10">
         <img className=" h-28 w-40 my-1" src={logo1} alt="" />
         <h1 className="font-bold text-white my-10 text-lg">Elite Emporium</h1>
       </span>
