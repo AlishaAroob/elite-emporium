021f3df078ed7e1ac6366c32507a14cc187fe142
AlishaAroob <aroob.dev@gmail.com>
2024-06-05 10:36:14 +0500

    adjusting margin

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,16 +7,20 @@ export default function Contact() {
         {" "}
         Contact Us
       </h1>
-      <span className="flex  space-x-2 ml-20 mt-10 p-10">
-        <img className=" h-28 w-40 my-1" src={logo1} alt="" />
-        <h1 className="font-bold text-white my-10 text-lg">Elite Emporium</h1>
-      </span>
-      <div className="text-2xl font-bold text-white ml-24 space-x-4 pl-16 ">
-        <p className=" ml-4 ">Follow us</p>
-        <br></br>
-        <i className="bx bxl-instagram "></i>
-        <i className="bx bxl-facebook-circle"></i>
-        <i className="bx bxl-twitter"></i>
+      <div className="grid grid-cols-3">
+        <div className=" space-x-2 ml-20 mt-10 p-10 ">
+          <h1 className="font-bold text-white my-10 text-lg">
+            <img className=" h-28 w-40 my-1" src={logo1} alt="" />
+            Elite Emporium
+          </h1>
+        </div>
+        <div className="text-2xl font-bold text-white ml-24 space-x-4 pl-16 ">
+          <p className=" ml-4 ">Follow us</p>
+          <br></br>
+          <i className="bx bxl-instagram "></i>
+          <i className="bx bxl-facebook-circle"></i>
+          <i className="bx bxl-twitter"></i>
+        </div>
       </div>
     </div>
   );
