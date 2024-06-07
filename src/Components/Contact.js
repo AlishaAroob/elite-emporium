52c13d3ba22ccfd46a6a5691ffece0a2a5e06358
AlishaAroob <aroob.dev@gmail.com>
2024-06-07 11:01:13 +0500

    styling contact box

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -7,19 +7,65 @@ export default function Contact() {
         {" "}
         Contact Us
       </h1>
-      <div className="grid grid-cols-3">
-        <div className=" space-x-2 ml-20 mt-8 p-10 ">
+      <div className="grid grid-cols-3 ">
+        <div className=" space-x-2 text-center ">
           <h1 className="font-bold text-white my-10 text-lg">
-            <img className=" h-28 w-40 my-1" src={logo1} alt="" />
+            <img className=" ml-36 h-28 w-40 text-center " src={logo1} alt="" />
             Elite Emporium
           </h1>
           <div className="text-xl font-bold text-white space-x-4 ">
-            <p className=" ml-4 ">Follow us</p>
+            <p className=" ml-4 cursor-pointer  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:text-yellow-500">
+              Follow us
+            </p>
             <br></br>
-            <i className="bx bxl-instagram "></i>
-            <i className="bx bxl-facebook-circle"></i>
-            <i className="bx bxl-twitter"></i>
+            <i className="bx bxl-instagram cursor-pointer  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:text-yellow-500"></i>
+            <i className="bx bxl-facebook-circle cursor-pointer  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:text-yellow-500"></i>
+            <i className="bx bxl-twitter cursor-pointer  transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:text-yellow-500"></i>
           </div>
+        </div>
+        <div className="flex flex-col text-white  text-center mt-24 space-y-2 font-bold ">
+          <a
+            className=" transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:text-yellow-500"
+            href="/"
+          >
+            About
+          </a>
+          <a
+            href="/"
+            className=" transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:text-yellow-500"
+          >
+            Support
+          </a>
+          <a
+            href="/"
+            className=" transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:text-yellow-500"
+          >
+            FAQS
+          </a>
+          <a
+            href="/"
+            className=" transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:text-yellow-500"
+          >
+            Become an Affiliate
+          </a>
+          <a
+            href="/"
+            className=" transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:text-yellow-500"
+          >
+            Gift Cards
+          </a>
+          <a
+            href="/"
+            className=" transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:text-yellow-500"
+          >
+            Return and Refund Policy
+          </a>
+          <a
+            href="/"
+            className=" transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300 hover:text-yellow-500"
+          >
+            Contact
+          </a>
         </div>
       </div>
     </div>
