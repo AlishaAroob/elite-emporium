3084fca60e6ce8b47b6e3090a7c9d51174c5e9ea
AlishaAroob <aroob.dev@gmail.com>
2024-06-08 13:03:57 +0500

    created Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <div>
-      <p></p>
+      <p>All rights reserved &#169;2024-Elite Emporium online book store</p>
     </div>
   );
 }
