5a6f28d89d3893ae779aacb46dfcaa196447e8d8
AlishaAroob <aroob.dev@gmail.com>
2024-06-21 22:16:50 +0500

    styling collection box

--- src/Components/AllCollection.js
+++ src/Components/AllCollection.js
@@ -4,7 +4,10 @@ export default function AllCollection() {
   return (
     <div className="container">
       <div className=" text-center font-bold text-4xl text-black ">
-        All Collection{" "}
+        All Collection
+        <div className="text-left m-5  text-2xl text-black ">
+          Trending Books
+        </div>
       </div>
     </div>
   );
