5f5098793eae6c7a20c732c0db9ff34bfed0dea7
AlishaAroob <aroob.dev@gmail.com>
2024-06-22 21:36:49 +0500

    adding text

--- src/Components/AllCollection.js
+++ src/Components/AllCollection.js
@@ -3,14 +3,13 @@ import React from "react";
 export default function AllCollection() {
   return (
     <div className="container">
-      <div className=" text-center font-bold text-4xl text-black ">
-        All Collection
-        <div className=" ">
-          <h2 className="text-left ml-5 mt-15 text-2xl text-black">
-            Trending Books
-          </h2>
-        </div>
-      </div>
+      <h1 className=" text-center font-bold text-4xl text-black ">
+        {" "}
+        All Collection{" "}
+      </h1>
+      <h2 className="text-left ml-5 mt-15 text-2xl text-black">
+        Trending Books
+      </h2>
     </div>
   );
 }
