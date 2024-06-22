306576e1154a6cd35a914c24e9fbae1d15878231
AlishaAroob <aroob.dev@gmail.com>
2024-06-22 21:34:01 +0500

    adding text in collection

--- src/Components/AllCollection.js
+++ src/Components/AllCollection.js
@@ -5,8 +5,10 @@ export default function AllCollection() {
     <div className="container">
       <div className=" text-center font-bold text-4xl text-black ">
         All Collection
-        <div className="text-left m-5  text-2xl text-black ">
-          Trending Books
+        <div className=" ">
+          <h2 className="text-left ml-5 mt-15 text-2xl text-black">
+            Trending Books
+          </h2>
         </div>
       </div>
     </div>
