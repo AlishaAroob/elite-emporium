da077ac7f6d1492b0e42674f886378c46ed6de70
AlishaAroob <aroob.dev@gmail.com>
2024-06-24 22:32:54 +0500

    Adding text

--- src/Components/AllCollection.js
+++ src/Components/AllCollection.js
@@ -10,6 +10,7 @@ export default function AllCollection() {
       <h2 className="text-left ml-5 mt-15 font-bold text-2xl text-black">
         Trending Books
       </h2>
+      <div className=""></div>
     </div>
   );
 }
