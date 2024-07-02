6eaea63f2262363e2794c53e8fd2009f19fe32e6
AlishaAroob <aroob.dev@gmail.com>
2024-07-02 22:06:29 +0500

    removed height and weight

--- src/Components/AllCollection.js
+++ src/Components/AllCollection.js
@@ -12,7 +12,7 @@ export default function AllCollection() {
         Trending Books
       </h2>
       <div className=" h-56   w-52  cursor-pointer transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300">
-        <img className="w-70 h-50 " src={Book2} alt="" />
+        <img className=" " src={Book2} alt="" />
       </div>
     </div>
   );
