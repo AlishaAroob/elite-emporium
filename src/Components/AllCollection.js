bc6e30f7ab7fc0ed0f03f8bf195a4535cc8faafa
AlishaAroob <aroob.dev@gmail.com>
2024-07-03 22:16:44 +0500

    Added collection

--- src/Components/AllCollection.js
+++ src/Components/AllCollection.js
@@ -15,6 +15,7 @@ export default function AllCollection() {
       <div className=" h-56   w-52 mt-5  cursor-pointer transition ease-in-out delay-150  hover:-translate-y-1 hover:scale-110 duration-300">
         <img className=" " src={Book2} alt="" />
         <img className=" w-64  px-5" src={Book1} alt="Book1" />
+        <img className=" w-64 px-5" src={Book} alt="Book" />
       </div>
     </div>
   );
